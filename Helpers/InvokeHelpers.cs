f2d2d30c9a080b3c40381ce9db96ce9b3147ac37
Komlos Boldizsar <komlos.boldizsar@gmail.com>
2024-07-28 14:18:53 +0200

    Add InvokeHelpers

---
+++ Helpers/InvokeHelpers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace BToolbox.GUI.Helpers
+{
+    public static class InvokeHelpers
+    {
+        public static void InvokeIfRequired(this Control control, Action action)
+        {
+            if (control.InvokeRequired)
+            {
+                control.Invoke(action);
+                return;
+            }
+            action();
+        }
+    }
+}
