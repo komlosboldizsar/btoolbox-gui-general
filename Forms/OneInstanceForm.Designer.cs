0dbb7b2f1ac4519571792c3d0ef3d6c82acb3649
Komlos Boldizsar <komlos.boldizsar@gmail.com>
2024-10-21 18:30:12 +0200

    Added OneInstanceForm

---
+++ Forms/OneInstanceForm.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace BToolbox.GUI.Forms
+{
+    partial class OneInstanceForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            trayIcon = new NotifyIcon(components);
+            trayMenu = new ContextMenuStrip(components);
+            trayMenuExit = new ToolStripMenuItem();
+            trayMenu.SuspendLayout();
+            SuspendLayout();
+            // 
+            // trayIcon
+            // 
+            trayIcon.ContextMenuStrip = trayMenu;
+            trayIcon.Text = "trayIcon";
+            trayIcon.Visible = true;
+            trayIcon.DoubleClick += trayIcon_DoubleClick;
+            // 
+            // trayMenu
+            // 
+            trayMenu.ImageScalingSize = new Size(20, 20);
+            trayMenu.Items.AddRange(new ToolStripItem[] { trayMenuExit });
+            trayMenu.Name = "contextMenuStrip1";
+            trayMenu.Size = new Size(211, 56);
+            // 
+            // trayMenuExit
+            // 
+            trayMenuExit.Name = "trayMenuExit";
+            trayMenuExit.Size = new Size(210, 24);
+            trayMenuExit.Text = "Exit";
+            trayMenuExit.Click += trayMenuExit_Click;
+            // 
+            // OneInstanceForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Name = "OneInstanceForm";
+            Text = "OneInstanceForm";
+            trayMenu.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        protected NotifyIcon trayIcon;
+        protected ContextMenuStrip trayMenu;
+        protected ToolStripMenuItem trayMenuExit;
+    }
+}
---
+++ Forms/OneInstanceForm.cs
@@ -0,0 +1,95 @@
+using BToolbox.GUI.Helpers;
+using BToolbox.OneInstance;
+using System.ComponentModel;
+
+namespace BToolbox.GUI.Forms
+{
+    public partial class OneInstanceForm : Form
+    {
+
+        public OneInstanceForm() => InitializeComponent();
+
+        public OneInstanceForm(bool oneInstanceMode, bool hideOnStartup)
+        {
+            InitializeComponent();
+            _oneInstanceMode = oneInstanceMode;
+            _hideOnStartup = hideOnStartup;
+            lastWindowStateNotMinimized = WindowState;
+            if (oneInstanceMode)
+            {
+                trayIcon.Visible = true;
+                OneInstanceGuard.ShowMessageReceived += OneInstanceShowMessageReceived;
+            }
+            if (hideOnStartup)
+            {
+                _hidingOnStartup = true;
+                ShowInTaskbar = false;
+                Opacity = 0;
+            }
+        }
+
+        #region One instance mode
+        private readonly bool _oneInstanceMode;
+        private readonly bool _hideOnStartup;
+        private bool _hidingOnStartup;
+
+        protected override bool ShowWithoutActivation => _hidingOnStartup;
+
+        protected override void OnLoad(EventArgs e)
+        {
+            if (_hideOnStartup)
+            {
+                Hide();
+                Opacity = 1;
+                ShowInTaskbar = true;
+                _hidingOnStartup = false;
+            }
+            base.OnLoad(e);
+        }
+
+        private void OneInstanceShowMessageReceived()
+            => this.InvokeIfRequired(() =>
+            {
+                Show();
+                WindowState = lastWindowStateNotMinimized;
+                Activate();
+            });
+        #endregion
+
+        #region Closing, minimizing, restoring
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            base.OnClosing(e);
+            if (_oneInstanceMode && !closingFromTrayMenu)
+            {
+                e.Cancel = true;
+                Hide();
+            }
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+            if (WindowState != FormWindowState.Minimized)
+                lastWindowStateNotMinimized = WindowState;
+        }
+
+        FormWindowState lastWindowStateNotMinimized;
+        #endregion
+
+        #region Tray
+        private void trayIcon_DoubleClick(object sender, EventArgs e)
+            => Show();
+
+        private void trayMenuExit_Click(object sender, EventArgs e)
+        {
+            closingFromTrayMenu = true;
+            Close();
+        }
+
+        bool closingFromTrayMenu = false;
+        #endregion
+
+
+    }
+}
