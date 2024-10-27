478507e1461cb26608f4d128ba42096c6eb1d079
Komlos Boldizsar <komlos.boldizsar@gmail.com>
2024-10-27 12:57:14 +0100

    Moved control cloning and table row/column cloning into this library

---
+++ RecyclerTables/MemberBinding.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace BToolbox.GUI.RecyclerTables
+{
+
+    public interface IMemberBinding
+    {
+        public void Set(Control[] controls);
+    }
+
+    public class MemberBinding<TControl> : IMemberBinding
+        where TControl : Control
+    {
+        private int columnIndex;
+        private Action<TControl> setter;
+        public MemberBinding(int columnIndex, Action<TControl> setter)
+        {
+            this.columnIndex = columnIndex;
+            this.setter = setter;
+        }
+        public void Set(Control[] controls)
+            => setter(controls[columnIndex] as TControl);
+    }
+
+}
